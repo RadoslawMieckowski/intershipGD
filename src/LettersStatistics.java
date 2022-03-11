fd65ca95b124665c1aa967325268f55a8bf263cd
rmieckowski <rmieckowski@griddynamics.com>
2022-03-11 15:25:57 +0100

    read line from scanner

--- src/LettersStatistics.java
+++ src/LettersStatistics.java
@@ -1,5 +1,12 @@
-public class LettersStatistics {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class LettersStatistics {
+    public static void main(String[] args) throws IOException {
+        File file = new File("src/input.txt");
+        Scanner in = new Scanner(file);
+        String line = in.nextLine();
+        System.out.println(line);
     }
 }
