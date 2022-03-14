6e840c0dc6e56512b1253e6365e6b0a8ab0d4eda
rmieckowski <rmieckowski@griddynamics.com>
2022-03-14 10:19:05 +0100

    create set of distinctive letters

--- src/LettersStatistics.java
+++ src/LettersStatistics.java
@@ -1,12 +1,15 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class LettersStatistics {
     public static void main(String[] args) throws IOException {
         File file = new File("src/input.txt");
         Scanner in = new Scanner(file);
-        String line = in.nextLine();
-        System.out.println(line);
+        String[] line = in.nextLine().toLowerCase().split("");
+        List<String> listOfLetters = Arrays.asList(line);
+        //System.out.println(list);
+        HashSet<String> distinctLetters = new HashSet<>(listOfLetters);
+        System.out.println(distinctLetters);
     }
 }
