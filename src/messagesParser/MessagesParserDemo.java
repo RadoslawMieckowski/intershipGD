911d93375d4b40b3b62ea1c9f0303c0fceb0cc80
rmieckowski <rmieckowski@griddynamics.com>
2022-03-22 11:16:18 +0100

    rename MessagesParser.java to MessagesParserDemo.java

--- src/messagesParser/MessagesParserDemo.java
+++ src/messagesParser/MessagesParserDemo.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class MessagesParser {
+public class MessagesParserDemo {
     static int wordPointer = 0;
     static String[] words;
     static int lengthLimit;
