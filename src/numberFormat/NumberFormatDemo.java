e108b43e54745ee0cc8b772ea7c76b2ef96dfcd0
rmieckowski <rmieckowski@griddynamics.com>
2022-03-22 12:09:27 +0100

    instantiate object of NumberFormatSolution and call it's method in main method of NumberFormatDemo.java file

--- src/numberFormat/NumberFormatDemo.java
+++ src/numberFormat/NumberFormatDemo.java
@@ -2,6 +2,7 @@ package numberFormat;
 
 public class NumberFormatDemo {
     public static void main(String[] args) {
-
+        NumberFormatSolution nfs = new NumberFormatSolution();
+        nfs.printSortedStrings();
     }
 }
