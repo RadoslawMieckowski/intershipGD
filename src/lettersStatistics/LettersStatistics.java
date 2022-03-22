631ebca65fc9b8dcb64dba33a036f6237f92a163
rmieckowski <rmieckowski@griddynamics.com>
2022-03-22 10:03:29 +0100

    replace static main method in LettersStatisticsSolution.java file with non-static printTopTenLetters method. Instantiate this class and call it's method in LettersStatistics.java file

--- src/lettersStatistics/LettersStatistics.java
+++ src/lettersStatistics/LettersStatistics.java
@@ -1,4 +1,11 @@
 package lettersStatistics;
 
 public class LettersStatistics {
+
+    public static void main(String[] args) {
+        LettersStatisticsSolution lss = new LettersStatisticsSolution();
+        lss.printTopTenLetters();
+    }
+
+
 }
--- src/lettersStatistics/LettersStatisticsSolution.java
+++ src/lettersStatistics/LettersStatisticsSolution.java
@@ -5,64 +5,63 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class LettersStatisticsSolution {
-    public static void main(String[] args) {
-        File file = new File("src/input.txt");
-        Scanner in = null;
-        try {
-            in = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            in.close();
-            e.printStackTrace();
-        }
+   public void printTopTenLetters() {
+       File file = new File("src/input.txt");
+       Scanner in = null;
+       try {
+           in = new Scanner(file);
+       } catch (FileNotFoundException e) {
+           in.close();
+           e.printStackTrace();
+       }
 
-        HashSet<String> distinctLetters = new HashSet<>();
-        List<String> listOfLetters = null;
-        while(in.hasNextLine()) {
-            String[] line = in.nextLine().toLowerCase().split("");
-            listOfLetters= Arrays.asList(line);
-            distinctLetters.addAll(listOfLetters);
-        }
-        //System.out.println("Before: " + distinctLetters);
+       HashSet<String> distinctLetters = new HashSet<>();
+       List<String> listOfLetters = null;
+       while (in.hasNextLine()) {
+           String[] line = in.nextLine().toLowerCase().split("");
+           listOfLetters = Arrays.asList(line);
+           distinctLetters.addAll(listOfLetters);
+       }
+       //System.out.println("Before: " + distinctLetters);
 
-        distinctLetters.removeIf(letter->
-                        letter.equals(" ")||
-                        letter.equals(",")||
-                        letter.equals(".")||
-                        letter.equals("?")||
-                        letter.equals("!")
-                );
-        //System.out.println("After: " + distinctLetters);
+       distinctLetters.removeIf(letter ->
+               letter.equals(" ") ||
+                       letter.equals(",") ||
+                       letter.equals(".") ||
+                       letter.equals("?") ||
+                       letter.equals("!")
+       );
+       //System.out.println("After: " + distinctLetters);
 
-        TreeMap<String, Long> mapOfDistinctLetters = new TreeMap<>();
-        long counter = 0;
-        for (String searchedLetter: distinctLetters) {
-            for (String letter: listOfLetters) {
-                if (searchedLetter.equals(letter)) {
-                    counter++;
-                }
-            }
-            mapOfDistinctLetters.put(searchedLetter, counter);
-            counter = 0;
-        }
+       TreeMap<String, Long> mapOfDistinctLetters = new TreeMap<>();
+       long counter = 0;
+       for (String searchedLetter : distinctLetters) {
+           for (String letter : listOfLetters) {
+               if (searchedLetter.equals(letter)) {
+                   counter++;
+               }
+           }
+           mapOfDistinctLetters.put(searchedLetter, counter);
+           counter = 0;
+       }
 
 //        Map<String, Long> mapOfDistinctLetters = distinctLetters.stream()
 //                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-        //System.out.println("unsorted map of letters: " + mapOfDistinctLetters);
+       //System.out.println("unsorted map of letters: " + mapOfDistinctLetters);
 
-        LinkedHashMap<String, Long> sortedMapOfLetters = new LinkedHashMap<>();
+       LinkedHashMap<String, Long> sortedMapOfLetters = new LinkedHashMap<>();
 
-        mapOfDistinctLetters.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .forEachOrdered(entry -> sortedMapOfLetters.put(entry.getKey(), entry.getValue()));
+       mapOfDistinctLetters.entrySet()
+               .stream()
+               .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+               .forEachOrdered(entry -> sortedMapOfLetters.put(entry.getKey(), entry.getValue()));
 
-        int limit = 0;
-        for (Map.Entry<String, Long> entry: sortedMapOfLetters.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-            limit++;
-            if (limit == 10) break;
-        }
-
-    }
+       int limit = 0;
+       for (Map.Entry<String, Long> entry : sortedMapOfLetters.entrySet()) {
+           System.out.println(entry.getKey() + ": " + entry.getValue());
+           limit++;
+           if (limit == 10) break;
+       }
+   }
 }
