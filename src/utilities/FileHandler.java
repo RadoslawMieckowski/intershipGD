e82ada6ddcbf8d4b135aba32df6ab4893df20487
rmieckowski <rmieckowski@griddynamics.com>
2022-03-23 13:27:57 +0100

    add convertToOneString method to FileHandler.java

--- src/utilities/FileHandler.java
+++ src/utilities/FileHandler.java
@@ -1,4 +1,30 @@
 package utilities;
 
-public class FileHandler {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public final class FileHandler {
+
+    private FileHandler(){}
+
+    public static String convertToOneString(String path) {
+        File file = new File(path);
+        Scanner in = null;
+        try {
+            in = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            in.close();
+            e.printStackTrace();
+        }
+        StringBuilder lineBuilder = new StringBuilder("");
+        while (in.hasNextLine()) {
+            lineBuilder.append(in.nextLine().toLowerCase());
+        }
+
+        String stringToReturn = lineBuilder.toString()
+                .replaceAll("[^a-z]", "");
+
+        return stringToReturn;
+    }
 }
