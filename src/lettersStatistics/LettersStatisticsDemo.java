e00187b1b94fb1dcacf782a73e991d7bcf3f3030
rmieckowski <rmieckowski@griddynamics.com>
2022-03-23 13:56:51 +0100

    add one-parameter constructor, change printTopTenLetter(use stream API to calculate letters from a string returned by a FileHandler utility class) in LettersStatisticsSolution.java

--- src/lettersStatistics/LettersStatisticsDemo.java
+++ src/lettersStatistics/LettersStatisticsDemo.java
@@ -3,7 +3,7 @@ package lettersStatistics;
 public class LettersStatisticsDemo {
 
     public static void main(String[] args) {
-        LettersStatisticsSolution lss = new LettersStatisticsSolution();
+        LettersStatisticsSolution lss = new LettersStatisticsSolution("src/data/input.txt");
         lss.printTopTenLetters();
     }
 
--- src/lettersStatistics/LettersStatisticsSolution.java
+++ src/lettersStatistics/LettersStatisticsSolution.java
@@ -1,56 +1,27 @@
 package lettersStatistics;
 
+import utilities.FileHandler;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LettersStatisticsSolution {
-   public void printTopTenLetters() {
-       File file = new File("src/data/input.txt");
-       Scanner in = null;
-       try {
-           in = new Scanner(file);
-       } catch (FileNotFoundException e) {
-           in.close();
-           e.printStackTrace();
-       }
+   private String inputFilePath;
+    public LettersStatisticsSolution(String inputFilePath) {
+       this.inputFilePath = inputFilePath;
+   }
+    public void printTopTenLetters() {
 
-       HashSet<String> distinctLetters = new HashSet<>();
-       List<String> listOfLetters = null;
-       while (in.hasNextLine()) {
-           String[] line = in.nextLine().toLowerCase().split("");
-           listOfLetters = Arrays.asList(line);
-           distinctLetters.addAll(listOfLetters);
-       }
-       //System.out.println("Before: " + distinctLetters);
+       String line = FileHandler.convertToOneString(inputFilePath);
 
-       distinctLetters.removeIf(letter ->
-               letter.equals(" ") ||
-                       letter.equals(",") ||
-                       letter.equals(".") ||
-                       letter.equals("?") ||
-                       letter.equals("!")
-       );
-       //System.out.println("After: " + distinctLetters);
+        Map<Character, Long> mapOfDistinctLetters = line.chars().mapToObj(c -> (char)c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-       TreeMap<String, Long> mapOfDistinctLetters = new TreeMap<>();
-       long counter = 0;
-       for (String searchedLetter : distinctLetters) {
-           for (String letter : listOfLetters) {
-               if (searchedLetter.equals(letter)) {
-                   counter++;
-               }
-           }
-           mapOfDistinctLetters.put(searchedLetter, counter);
-           counter = 0;
-       }
-
-//        Map<String, Long> mapOfDistinctLetters = distinctLetters.stream()
-//                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-
-       //System.out.println("unsorted map of letters: " + mapOfDistinctLetters);
-
-       LinkedHashMap<String, Long> sortedMapOfLetters = new LinkedHashMap<>();
+       LinkedHashMap<Character, Long> sortedMapOfLetters = new LinkedHashMap<>();
 
        mapOfDistinctLetters.entrySet()
                .stream()
@@ -58,7 +29,7 @@ public class LettersStatisticsSolution {
                .forEachOrdered(entry -> sortedMapOfLetters.put(entry.getKey(), entry.getValue()));
 
        int limit = 0;
-       for (Map.Entry<String, Long> entry : sortedMapOfLetters.entrySet()) {
+       for (Map.Entry<Character, Long> entry : sortedMapOfLetters.entrySet()) {
            System.out.println(entry.getKey() + ": " + entry.getValue());
            limit++;
            if (limit == 10) break;
