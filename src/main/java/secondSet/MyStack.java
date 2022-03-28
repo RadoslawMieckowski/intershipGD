e8018e595f96587112d8a31e30bef8c611a38882
rmieckowski <rmieckowski@griddynamics.com>
2022-03-28 20:41:39 +0200

    add size, push, pop, peek, iterator methods in MyStack.java

--- src/main/java/secondSet/MyStack.java
+++ src/main/java/secondSet/MyStack.java
@@ -1,4 +1,27 @@
 package secondSet;
 
-public class MyStack {
+import java.util.Iterator;
+
+public class MyStack<E> {
+
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    public E peek() {
+        return null;
+    }
+
+    public E pop() {
+        return null;
+    }
+
+    public E push(E element) {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
 }
