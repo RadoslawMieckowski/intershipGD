11f313191f36950c98be741b8e2d49e098863343
rmieckowski <rmieckowski@griddynamics.com>
2022-03-29 19:07:20 +0200

    add wordsAndLimitArray array in MessagesParserSolution.java

--- src/main/java/messagesParser/MessagesParserSolution.java
+++ src/main/java/messagesParser/MessagesParserSolution.java
@@ -2,6 +2,7 @@ package messagesParser;
 
 import utilities.FileHandler;
 
+import java.util.Arrays;
 
 
 public class MessagesParserSolution {
@@ -9,6 +10,7 @@ public class MessagesParserSolution {
     private String inputFilePath;
     private int lengthLimit;
     private String[] words;
+    private String[] wordsAndLimitArray;
 
     public MessagesParserSolution(String inputFilePath) {
         this.inputFilePath = inputFilePath;
@@ -26,24 +28,25 @@ public class MessagesParserSolution {
     }
 
     private void setWords() {
-        words = FileHandler.convertToOneString(inputFilePath)
+        wordsAndLimitArray = FileHandler.convertToOneString(inputFilePath)
                 .toString()
                 .split(" ");
+        words = Arrays.copyOf(wordsAndLimitArray, wordsAndLimitArray.length-1);
     }
 
     private void setLengthLimit() {
-        lengthLimit = Integer.parseInt(words[words.length-1]);
+        lengthLimit = Integer.parseInt(wordsAndLimitArray[wordsAndLimitArray.length-1]);
     }
 
     private void printLines() {
         StringBuilder line = new StringBuilder();
-        while (wordPointer < words.length) {
+        while (wordPointer <= words.length) {
             if (line.length() > lengthLimit) {
                 System.out.println(line);
                 printLines();
                 return;
             } else {
-                if (wordPointer == words.length - 1) {
+                if (wordPointer == words.length) {
                     System.out.println(line);
                     return;
                 }
