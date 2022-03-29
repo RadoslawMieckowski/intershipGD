8efd9cae8de96ec8bacab6cd01308f27573d1d85
rmieckowski <rmieckowski@griddynamics.com>
2022-03-29 17:29:06 +0200

    add throwing exceptions in push method of MyStack.java

--- src/main/java/secondSet/MyStack.java
+++ src/main/java/secondSet/MyStack.java
@@ -4,6 +4,7 @@ import java.util.EmptyStackException;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import static java.lang.Integer.valueOf;
 
 public class MyStack<Integer> {
 private List<Integer> innerList;
@@ -28,14 +29,31 @@ public MyStack(int size) {
         return ((LinkedList<Integer>) innerList).pop();
     }
 
-    public Integer push(Integer element) {
+    public int push(int element) {
     if (innerList.size() == size) throw new FullMyStackException("The stack is already full!");
-        ((LinkedList<Integer>) innerList).push(element);
+    if (isPrimeNumber(element) == false) throw new NotPrimeNumberException("Given number is not a prime one!");
+    if (innerList.size() == 0) {
+        ((LinkedList<Integer>) innerList).push((Integer) valueOf(element));
+    } else {
+        if (element <= ((int)innerList.get(0))) throw new SmallerNumberException("Given number is smaller than one on the top of the stack!");
+        ((LinkedList<Integer>) innerList).push((Integer) valueOf(element));
+    }
         return element;
     }
 
     public int size() {
         return innerList.size();
+    }
+
+    private boolean isPrimeNumber(int number) {
+        boolean flag = true;
+        for(int i = 2; i < number; i++){
+            if (number % i == 0){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
     }
 
 }
--- src/main/java/secondSet/MyStackDemo.java
+++ src/main/java/secondSet/MyStackDemo.java
@@ -6,8 +6,8 @@ public class MyStackDemo {
     public static void main(String[] args) {
         MyStack myStack = new MyStack<>(3);
         myStack.push(1);
-        myStack.push(3);
         myStack.push(2);
+        myStack.push(3);
         myStack.pop();
         Iterator<Integer> iterator = myStack.iterator();
         while (iterator.hasNext()) {
