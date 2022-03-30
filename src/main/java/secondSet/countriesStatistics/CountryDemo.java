d10e0b5e5d29b51e31f60161dcefe08d8880843a
rmieckowski <rmieckowski@griddynamics.com>
2022-03-30 14:15:13 +0200

    add "final" modifier and private constructor to CSVReader utility class

--- src/main/java/secondSet/countriesStatistics/CountryDemo.java
+++ src/main/java/secondSet/countriesStatistics/CountryDemo.java
@@ -1,7 +1,9 @@
 package secondSet.countriesStatistics;
 
+import secondSet.utilities.CSVReader;
+
 public class CountryDemo {
     public static void main(String[] args) {
-
+        CSVReader.readCSVFile("src/main/resources/data/countriesStatistics.csv");
     }
 }
--- src/main/java/secondSet/utilities/CSVReader.java
+++ src/main/java/secondSet/utilities/CSVReader.java
@@ -1,37 +1,15 @@
 package secondSet.utilities;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
-public class CSVReader {
-    private static List<String> entries;
-    public static void main(String[] args) {
-        readCSVFile("src/main/resources/data/countriesStatistics.csv");
-    }
-
-    public static List<String> readCSVFile2(String filePath) {
-        List<String> entries = new ArrayList<>();
-        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while((line = reader.readLine()) != null) {
-                entries.add(line);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("entries: " + entries);
-        return entries;
-    }
+public final class CSVReader {
+    private CSVReader(){}
 
     public static List<String> readCSVFile(String filePath) {
+        List<String> entries = null;
         try {
             entries = Files.lines(Paths.get(filePath))
                     .skip(1)
