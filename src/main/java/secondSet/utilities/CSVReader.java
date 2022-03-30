7899afc733bffaf4ed4f2e2376a200a44c4375ee
rmieckowski <rmieckowski@griddynamics.com>
2022-03-30 14:02:16 +0200

    rename readCSVFile method to readCSVFile2 and add static readCSVFile method in CSVReader.java, add column names in countriesStatistics.csv

--- src/main/java/secondSet/utilities/CSVReader.java
+++ src/main/java/secondSet/utilities/CSVReader.java
@@ -4,15 +4,18 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CSVReader {
+    private static List<String> entries;
     public static void main(String[] args) {
         readCSVFile("src/main/resources/data/countriesStatistics.csv");
     }
 
-    public static List<String> readCSVFile(String filePath) {
+    public static List<String> readCSVFile2(String filePath) {
         List<String> entries = new ArrayList<>();
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
@@ -25,6 +28,18 @@ public class CSVReader {
             e.printStackTrace();
         }
         System.out.println("entries: " + entries);
+        return entries;
+    }
+
+    public static List<String> readCSVFile(String filePath) {
+        try {
+            entries = Files.lines(Paths.get(filePath))
+                    .skip(1)
+                    .toList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(entries);
         return entries;
     }
 }
