08913c7ad11d85895388a9b9cf32820f0abb7b2e
rmieckowski <rmieckowski@griddynamics.com>
2022-03-30 11:52:22 +0200

    rename file input.txt to lettersStatistics.txt

--- src/main/java/firstSet/lettersStatistics/LettersStatisticsDemo.java
+++ src/main/java/firstSet/lettersStatistics/LettersStatisticsDemo.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class LettersStatisticsDemo {
 
     public static void main(String[] args) {
-        LettersStatisticsSolution lettersStatisticsSolution1 = new LettersStatisticsSolution("src/main/resources/data/input.txt");
+        LettersStatisticsSolution lettersStatisticsSolution1 = new LettersStatisticsSolution("src/main/resources/data/lettersStatistics.txt");
         String line = lettersStatisticsSolution1.readLineFromFile();
         Map<Character, Long> mapOfDistinctLetters = lettersStatisticsSolution1.createMapOfDistinctLetters(line);
         Map<Character, Long> sortedMapOfLetters = lettersStatisticsSolution1.createSortedMapOfLetters(mapOfDistinctLetters);
