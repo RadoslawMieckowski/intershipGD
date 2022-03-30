120762f9f0d0e269a0fa34e050805d42307b5210
rmieckowski <rmieckowski@griddynamics.com>
2022-03-30 13:41:11 +0200

    add readCSVFile static method to CSVReader.java

--- src/main/java/secondSet/utilities/CSVReader.java
+++ src/main/java/secondSet/utilities/CSVReader.java
@@ -1,4 +1,30 @@
 package secondSet.utilities;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CSVReader {
+    public static void main(String[] args) {
+        readCSVFile("src/main/resources/data/countriesStatistics.csv");
+    }
+
+    public static List<String> readCSVFile(String filePath) {
+        List<String> entries = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                entries.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("entries: " + entries);
+        return entries;
+    }
 }
