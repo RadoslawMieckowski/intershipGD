c18814485bb0d4650d4f50d8ed6272ae4523fb61
rmieckowski <rmieckowski@griddynamics.com>
2022-04-02 01:00:22 +0200

    optimize imports in Country.java

--- src/main/java/secondSet/countriesStatistics/Country.java
+++ src/main/java/secondSet/countriesStatistics/Country.java
@@ -1,8 +1,10 @@
 package secondSet.countriesStatistics;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Country {
