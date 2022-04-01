954528e19de4f68bec3278f1f998a5ed06fa0173
rmieckowski <rmieckowski@griddynamics.com>
2022-04-02 00:45:14 +0200

    add FlatteningIterator to iterators directory, implementing next, hasNext getStreamFromIterator methods in FlatteningIterator.java modifying FlatteningIteratorDemo.java

---
+++ src/main/java/secondSet/iterators/FlatteningIterator.java
@@ -0,0 +1,37 @@
+package secondSet.iterators;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class FlatteningIterator<T> implements Iterator {
+
+    private List<T> listOfIterators;
+    private Iterator<T> iterator;
+
+    public FlatteningIterator(Iterator<T>... iterators) {
+        listOfIterators = Arrays.stream(iterators)
+                .flatMap(iterator ->getStreamFromIterator(iterator))
+                .collect(Collectors.toList());
+        iterator = listOfIterators.iterator();
+    }
+
+    private Stream<T> getStreamFromIterator(Iterator<T> iterator) {
+        Spliterator<T> spliterator =
+                Spliterators.spliteratorUnknownSize(iterator, 0);
+        return StreamSupport.stream(spliterator, false);
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (iterator.hasNext())
+        return true;
+        return false;
+    }
+
+    @Override
+    public T next() {
+        return iterator.next();
+    }
+}
--- src/main/java/secondSet/iterators/demo/FlatteningIteratorDemo.java
+++ src/main/java/secondSet/iterators/demo/FlatteningIteratorDemo.java
@@ -1,4 +1,21 @@
 package secondSet.iterators.demo;
 
+import secondSet.iterators.FlatteningIterator;
+
+import java.util.Iterator;
+import java.util.List;
+
 public class FlatteningIteratorDemo {
+    public static void main(String[] args) {
+        Iterator<Integer>iter = new FlatteningIterator<Integer>(
+                List.of(42, 5).iterator(),
+                List.of(-4).iterator(),
+                List.of(999, 998, 997).iterator()
+        );
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+
+
+    }
 }
