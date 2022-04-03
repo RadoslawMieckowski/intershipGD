d8236d9bfff12161b46a94f31306cdd1a82488fa
rmieckowski <rmieckowski@griddynamics.com>
2022-04-04 01:17:22 +0200

    fix name of class in BusinessDaysIteratorDemo.java

---
+++ src/main/java/secondSet/iterators/demo/BusinessDaysIteratorDemo.java
@@ -0,0 +1,14 @@
+package secondSet.iterators.demo;
+
+import java.time.LocalDate;
+import java.util.Iterator;
+
+public class BusinessDaysIteratorDemo {
+    public static void main(String[] args) {
+        Iterator<LocalDate> iter = new secondSet.iterators.BusinessDaysIterator<>(
+                LocalDate.of(2022, 1, 1)
+        );
+        System.out.println(iter.next());//fix format!
+        System.out.println(iter.next());
+    }
+}
