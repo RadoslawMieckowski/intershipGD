de0ea99b08eeeb201f5cc06ff73e2a7a9a24a769
rmieckowski <rmieckowski@griddynamics.com>
2022-04-04 13:31:35 +0200

    add nameList,ageList,personList fields in ZippingIterator.java

--- src/main/java/secondSet/iterators/ZippingIterator.java
+++ src/main/java/secondSet/iterators/ZippingIterator.java
@@ -1,4 +1,24 @@
 package secondSet.iterators;
 
-public class ZippingIterator {
+import java.util.Iterator;
+import java.util.List;
+
+public class ZippingIterator<E> implements Iterator<Person>{
+    private List<String> nameList;
+    private List<Integer> ageList;
+    private List<Person> personList;
+
+    public ZippingIterator(Iterator<String> nameListIterator, Iterator<Integer> ageListIterator) {
+
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public E next() {
+        return null;
+    }
 }
--- src/main/java/secondSet/iterators/demo/ZippingIteratorDemo.java
+++ src/main/java/secondSet/iterators/demo/ZippingIteratorDemo.java
@@ -1,4 +1,7 @@
 package secondSet.iterators.demo;
 
 public class ZippingIteratorDemo {
+    public static void main(String[] args) {
+
+    }
 }
