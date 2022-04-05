590eaedbb6bfb500bfe6740a0c8906c77d567f83
rmieckowski <rmieckowski@griddynamics.com>
2022-04-05 12:03:42 +0200

    add MyStackTest.java to test directory

---
+++ src/test/java/secondSet/stack/MyStackTest.java
@@ -0,0 +1,28 @@
+package secondSet.stack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyStackTest {
+
+    @Test
+    void iterator() {
+    }
+
+    @Test
+    void peek() {
+    }
+
+    @Test
+    void pop() {
+    }
+
+    @Test
+    void push() {
+    }
+
+    @Test
+    void size() {
+    }
+}
