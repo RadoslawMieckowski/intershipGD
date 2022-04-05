b15fb2a8792766f8eb681d68e58edee00d585193
rmieckowski <rmieckowski@griddynamics.com>
2022-04-05 23:06:58 +0200

    add CountryTest.java to test directory

---
+++ src/test/java/secondSet/countriesStatistics/CountryTest.java
@@ -0,0 +1,48 @@
+package secondSet.countriesStatistics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryTest {
+
+    @Test
+    void sortByPopulationDesc() {
+    }
+
+    @Test
+    void findCountryWithMaxPopulation() {
+    }
+
+    @Test
+    void findCountryWithMinPopulation() {
+    }
+
+    @Test
+    void findCountryWithMaxArea() {
+    }
+
+    @Test
+    void findCountryWithMinArea() {
+    }
+
+    @Test
+    void sortByContinentAndArea() {
+    }
+
+    @Test
+    void findCountryWithMaxPopulationOnContinent() {
+    }
+
+    @Test
+    void sumOfAreaOfContinent() {
+    }
+
+    @Test
+    void deleteCountriesWithPopulationOverThreshold() {
+    }
+
+    @Test
+    void listToMap() {
+    }
+}
