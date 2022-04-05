f3d791e19b188bc3733cd0d64423526c308771d5
rmieckowski <rmieckowski@griddynamics.com>
2022-04-05 18:32:04 +0200

    add BusinessDaysIteratorTest.java to test directory

---
+++ src/test/java/secondSet/iterators/BusinessDaysIteratorTest.java
@@ -0,0 +1,16 @@
+package secondSet.iterators;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BusinessDaysIteratorTest {
+
+    @Test
+    void hasNext() {
+    }
+
+    @Test
+    void next() {
+    }
+}
