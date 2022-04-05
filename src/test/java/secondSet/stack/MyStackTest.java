9b75384cf50663dd427941531a8c6dcb7e7699cd
rmieckowski <rmieckowski@griddynamics.com>
2022-04-05 14:05:23 +0200

    add FullMyStackExceptionTesting and push methods in MyStackTest.java

--- src/test/java/secondSet/stack/MyStackTest.java
+++ src/test/java/secondSet/stack/MyStackTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import secondSet.stack.stackExceptions.FullMyStackException;
 
 import java.util.Iterator;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MyStackTest {
@@ -33,18 +35,34 @@ class MyStackTest {
     void pop() {
     }
 
-    @RepeatedTest(4)
-    @DisplayName("pushing elements to myStack instance should work")
+    @Test
+    @DisplayName("pushing primes increasing elements to myStack instance should work")
     void push() {
+        assertEquals(myStack.push(2), 2, "Regular pushing should work");
+        assertEquals(myStack.push(3), 3, "Regular pushing should work");
+        assertEquals(myStack.push(5), 5, "Regular pushing should work");
         assertEquals(myStack.push(7), 7, "Regular pushing should work");
     }
 
     @Test
+    void FullMyStackExceptionTesting() {
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(5);
+        myStack.push(7);
+
+        Throwable exception = assertThrows(FullMyStackException.class, () -> myStack.push(11));
+        assertEquals("The stack is already full!", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("checking the size of MyStack instance should work")
     void size() {
         myStack.push(2);
         myStack.push(3);
         myStack.push(5);
         myStack.push(7);
+
         assertEquals( 4, myStack.size(), "size() method should work");
     }
 }
