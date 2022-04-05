c008ecc4a38c73699b3195e651bfe1ab24c64b8a
rmieckowski <rmieckowski@griddynamics.com>
2022-04-05 20:50:42 +0200

    add ZippingIteratorTest.java to test directory

---
+++ src/test/java/secondSet/iterators/ZippingIteratorTest.java
@@ -0,0 +1,16 @@
+package secondSet.iterators;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZippingIteratorTest {
+
+    @Test
+    void hasNext() {
+    }
+
+    @Test
+    void next() {
+    }
+}
