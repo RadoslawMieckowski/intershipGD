7402fb23db026be939044bcdac611a0fa803e320
rmieckowski <rmieckowski@griddynamics.com>
2022-04-12 20:10:08 +0200

    removing redundant methods in BufferingIterator.java, modify BufferingIteratorDemo.java

--- src/main/java/secondSet/iterators/BufferingIterator.java
+++ src/main/java/secondSet/iterators/BufferingIterator.java
@@ -1,11 +1,8 @@
 package secondSet.iterators;
 
-import secondSet.utilities.IteratorHandler;
-
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class BufferingIterator<E> implements Iterator<List<E>> {
     private List<List<E>> listOfLists;
@@ -14,22 +11,17 @@ public class BufferingIterator<E> implements Iterator<List<E>> {
     private int addedElements = 0;
 
     public BufferingIterator(Iterator<E> iterator, int batchSize) {
-        List<E> copiedList = createList(iterator);
         this.batchSize = batchSize;
-        listOfLists = createListOfLists(copiedList ,copiedList.iterator());
+        listOfLists = createListOfLists(iterator);
         listIterator = listOfLists.iterator();
     }
 
-    private List<E> createList(Iterator<E> iterator) {
-        List<E> copiedList = IteratorHandler
-                .getStreamFromIterator(iterator)
-                .collect(Collectors.toList());
-        return copiedList;
-    }
-
-    private int getNumberOfSubLists (List<E> list) {
-        return list.size() % batchSize == 0 ?
-                list.size()/batchSize : (list.size()/batchSize) + 1;
+    private List<List<E>> createListOfLists(Iterator<E> iterator) {
+        listOfLists = new LinkedList<>();
+        while (iterator.hasNext()) {
+            listOfLists.add(fillSubList(iterator));
+        }
+        return listOfLists;
     }
 
     private List<E> fillSubList(Iterator<E> iterator) {
@@ -40,18 +32,6 @@ public class BufferingIterator<E> implements Iterator<List<E>> {
         }
         addedElements = 0;
         return subList;
-    }
-
-    private List<List<E>> createListOfLists(List<E> list, Iterator<E> iterator) {
-        listOfLists = new LinkedList<>();
-        int numberOfLists = getNumberOfSubLists(list);
-        List<E> subList;
-        while (numberOfLists > 0) {
-            subList = fillSubList(iterator);
-            numberOfLists--;
-            listOfLists.add(subList);
-        }
-        return listOfLists;
     }
 
     @Override
--- src/main/java/secondSet/iterators/demo/BufferingIteratorDemo.java
+++ src/main/java/secondSet/iterators/demo/BufferingIteratorDemo.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class BufferingIteratorDemo {
     public static void main(String[] args) {
         Iterator<List<Integer>> iter = new BufferingIterator<>(
-                List.of(1, 2, 3, 4, 5).iterator(), 3
+                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 0).iterator(), 4
         );
         while (iter.hasNext()) {
             List<Integer> batch = iter.next();
