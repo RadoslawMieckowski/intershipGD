c34e28c42f0e0dc0913b318bc0da8a604bc5623b
rmieckowski <rmieckowski@griddynamics.com>
2022-04-20 23:40:40 +0200

    add Intern.java to pojos directory, change JsonAttribute.java

---
+++ src/main/java/thirdSet/annotations/pojos/Intern.java
@@ -0,0 +1,36 @@
+package thirdSet.annotations.pojos;
+
+import thirdSet.annotations.JsonAttribute;
+import thirdSet.annotations.JsonSerializable;
+
+@JsonSerializable
+public class Intern {
+    @JsonAttribute(jsonFieldName = "internName")
+    private String name;
+    @JsonAttribute
+    private String surName;
+
+    public Intern(String name, String surName) {
+        this.name = name;
+        this.surName = surName;
+    }
+
+    public Intern() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+}
