370cf0f97299a9250de762961beaa8345b868087
rmieckowski <rmieckowski@griddynamics.com>
2022-04-21 11:48:52 +0200

    rename SerializationClient.java to JsonSerializer.java, add serializePojoObject, createAttributesMap, mapToJson methods in JsonSerializer.java

---
+++ src/main/java/thirdSet/JsonSerializer.java
@@ -0,0 +1,60 @@
+package thirdSet;
+
+import thirdSet.annotations.JsonAttribute;
+import thirdSet.annotations.JsonSerializable;
+import thirdSet.exceptions.JsonSerializationException;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class JsonSerializer {
+
+    public JsonSerializer() {}
+
+    public void serializePojoObject(Object pojoObject) {
+        checkIfSerializable(pojoObject);
+        Map<String, String> attributesMap = createAttributesMap(pojoObject);
+        String jsonObject = mapToJson(attributesMap);
+        System.out.println(jsonObject);
+    }
+
+    private void checkIfSerializable(Object object) {
+        if (Objects.isNull(object)) {
+            throw new JsonSerializationException("The object to serialize is null");
+        }
+        Class<?> aClass = object.getClass();
+        if (!aClass.isAnnotationPresent(JsonSerializable.class)) {
+            throw new JsonSerializationException("The class "
+                    + aClass.getSimpleName()
+                    + " is not annotated with JsonSerializable");
+        }
+    }
+
+    private Map<String, String> createAttributesMap(Object object) {
+        Class<?> aClass = object.getClass();
+        Map<String, String> attributesMap = new HashMap<>();
+        for (Field field : aClass.getDeclaredFields()) {
+            field.setAccessible(true);
+            if (field.isAnnotationPresent(JsonAttribute.class)) {
+                try {
+                    attributesMap.put(field.getName(), (String) field.get(object));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return attributesMap;
+    }
+
+    private String mapToJson(Map<String, String> attributesMap) {
+        String jsonString = attributesMap.entrySet()
+                .stream()
+                .map(entry -> "\"" + entry.getKey() + "\":\""
+                        + entry.getValue() + "\"")
+                .collect(Collectors.joining(","));
+        return "{" + jsonString + "}";
+    }
+}
