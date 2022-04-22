b9a3b0c7b7907ca937c46fcb1a09d7052ba2f799
rmieckowski <rmieckowski@griddynamics.com>
2022-04-22 15:09:14 +0200

    add mapProxyInstance in main of Demo.java

--- src/main/java/thirdSet/dynamicProxy/demos/Demo.java
+++ src/main/java/thirdSet/dynamicProxy/demos/Demo.java
@@ -1,7 +1,19 @@
 package thirdSet.dynamicProxy.demos;
 
+import thirdSet.dynamicProxy.handlers.StopwatchInvocationHandler;
+
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Demo {
     public static void main(String[] args) {
 
+        Map mapProxyInstance = (Map) Proxy.newProxyInstance(
+                StopwatchInvocationHandler.class.getClassLoader(), new Class[] { Map.class },
+                new StopwatchInvocationHandler(new HashMap<>()));
+
+        mapProxyInstance.putAll(Map.of(1,"one",2, "two",3, "three"));
+        mapProxyInstance.get("two");
     }
 }
