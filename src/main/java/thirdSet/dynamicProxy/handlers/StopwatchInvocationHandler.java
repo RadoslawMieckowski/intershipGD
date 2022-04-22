12d8914d705235e982bf8757d933bfd173749315
rmieckowski <rmieckowski@griddynamics.com>
2022-04-22 14:46:24 +0200

    change invoke method in StopwatchInvocationHandler.java

--- src/main/java/thirdSet/dynamicProxy/handlers/StopwatchInvocationHandler.java
+++ src/main/java/thirdSet/dynamicProxy/handlers/StopwatchInvocationHandler.java
@@ -4,8 +4,18 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class StopwatchInvocationHandler implements InvocationHandler {
+    private Object target;
+
+    public StopwatchInvocationHandler(Object target) {
+        this.target = target;
+    }
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        return null;
+        long start = System.nanoTime();
+        Object result = method.invoke( target, args);
+        long elapsed = System.nanoTime() - start;
+        System.out.println("Executing " + method.getName() + " took " + elapsed + " nanos.");
+        return result;
     }
 }
