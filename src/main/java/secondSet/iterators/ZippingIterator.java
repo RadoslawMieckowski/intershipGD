a58aeb89bfa53e73ed527acc2483c8d1aa3801a7
rmieckowski <rmieckowski@griddynamics.com>
2022-04-22 21:36:36 +0200

    add defineSizeOfPersonList, change constructor of ZippingIterator.java

--- src/main/java/secondSet/iterators/ZippingIterator.java
+++ src/main/java/secondSet/iterators/ZippingIterator.java
@@ -1,6 +1,7 @@
 package secondSet.iterators;
 
 import secondSet.iterators.models.Person;
+import secondSet.utilities.IteratorHandler;
 
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -10,15 +11,27 @@ import java.util.function.BiFunction;
 public class ZippingIterator<String, Integer> implements Iterator<Person>{
     private List<Person> personList;
     private Iterator<Person> personListIterator;
+    private List<String> nameList;
+    private List<Integer> ageList;
 
     public ZippingIterator(Iterator<String> nameIterator,
                            Iterator<Integer> ageIterator,
                            BiFunction<String, Integer, Person> combiner) {
         personList = new LinkedList<>();
-        while (nameIterator.hasNext() && ageIterator.hasNext()) {
-            personList.add(combiner.apply(nameIterator.next(), ageIterator.next()));
+        nameList = IteratorHandler.getStreamFromIterator(nameIterator)
+                .toList();
+        ageList = IteratorHandler.getStreamFromIterator(ageIterator)
+                .toList();
+        int sizePersonList = defineSizeOfPersonList();
+        for(int i = 0; i < sizePersonList; i++) {
+            personList.add(combiner.apply(nameList.get(i), ageList.get(i)));
         }
         personListIterator = personList.iterator();
+    }
+
+    private int defineSizeOfPersonList() {
+        if (nameList.size() < ageList.size()) return nameList.size();
+        else return ageList.size();
     }
 
     @Override
