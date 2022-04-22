ec157087568e8b4f151afea62b740cc99b120270
rmieckowski <rmieckowski@griddynamics.com>
2022-04-22 14:38:55 +0200

    add StopwatchInvocationHandler.java to handlers directory

---
+++ src/main/java/thirdSet/dynamicProxy/handlers/StopwatchInvocationHandler.java
@@ -0,0 +1,11 @@
+package thirdSet.dynamicProxy.handlers;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class StopwatchInvocationHandler implements InvocationHandler {
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
