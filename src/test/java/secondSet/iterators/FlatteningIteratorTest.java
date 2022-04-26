ec88e39a59dadf0de799c24aedf5486094531362
rmieckowski <rmieckowski@griddynamics.com>
2022-04-26 16:25:18 +0200

    remove setUp method, change test methods in FlatteningIteratorTest.java

--- src/test/java/secondSet/iterators/FlatteningIteratorTest.java
+++ src/test/java/secondSet/iterators/FlatteningIteratorTest.java
@@ -1,36 +1,21 @@
 package secondSet.iterators;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-import java.util.EmptyStackException;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FlatteningIteratorTest {
-
-    FlatteningIterator<Integer> flatteningIterator;
-
-    @BeforeEach
-    void setUp() {
-        List<Integer> list1 = List.of();
-        List<Integer> list2 = List.of();
-        flatteningIterator = new FlatteningIterator<Integer>(
-                list1.iterator(),
-                list2.iterator()
-        );
-    }
 
     @Test
     @DisplayName("hasNext method should return all elements")
     void next() {
-        flatteningIterator = new FlatteningIterator<>(
+        FlatteningIterator<Integer> flatteningIterator = new FlatteningIterator<>(
                 List.of(1, 2).iterator(),
                 List.of(3, 4, 5).iterator(),
                 List.of(6, 7, 8, 9).iterator()
@@ -47,12 +32,26 @@ class FlatteningIteratorTest {
     @Test
     @DisplayName("hasNext method should return false if iterator has nothing to iterate on!")
     void hasNextOnEmptyIterator() {
+        List<Integer> list1 = List.of();
+        List<Integer> list2 = List.of();
+        FlatteningIterator<Integer> flatteningIterator = new FlatteningIterator<>(
+                list1.iterator(),
+                list2.iterator()
+        );
+
         assertTrue("hasNext method should return false if iterator has nothing to iterate on!",
                 flatteningIterator.hasNext() == false);
     }
 
     @Test
     void nextOnEmptyIterator() {
+        List<Integer> list1 = List.of();
+        List<Integer> list2 = List.of();
+        FlatteningIterator<Integer> flatteningIterator = new FlatteningIterator<>(
+                list1.iterator(),
+                list2.iterator()
+        );
+
         Throwable exception = assertThrows(NoSuchElementException.class,
                 () -> flatteningIterator.next());
         assertTrue(exception instanceof NoSuchElementException);
