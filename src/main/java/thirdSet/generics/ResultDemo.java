94300473b279ef94a1f25e43783f8601e33acaf8
rmieckowski <rmieckowski@griddynamics.com>
2022-04-28 18:11:22 +0200

    instatniate Result instances in ResultDemo.java

--- src/main/java/thirdSet/generics/ResultDemo.java
+++ src/main/java/thirdSet/generics/ResultDemo.java
@@ -1,6 +1,18 @@
 package thirdSet.generics;
 
-public class ResultDemo {
-    Result<Integer, Exception> result = Result.ok(3);
+import java.time.LocalDateTime;
+import java.util.function.Supplier;
 
+public class ResultDemo {
+    public static void main(String[] args) {
+        Result<Integer, Exception> resultOk = Result.ok(3);
+
+        Result<Integer, Exception> resultException = Result.err(new Exception());
+
+        Supplier<LocalDateTime> supplierValue = () -> LocalDateTime.now();
+        Result<LocalDateTime, ?> resultOfValue = Result.of(supplierValue);
+
+        Supplier<Integer> supplierException = () -> 3/0;
+        Result<Integer, ?> resultOfException = Result.of(supplierException);
+    }
 }
