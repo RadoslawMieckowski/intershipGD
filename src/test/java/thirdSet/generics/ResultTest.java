cf7d7d092bd4621cba96431fe6154d6645e658f9
rmieckowski <rmieckowski@griddynamics.com>
2022-04-28 19:00:09 +0200

    add ResultTest.java to test directory

---
+++ src/test/java/thirdSet/generics/ResultTest.java
@@ -0,0 +1,36 @@
+package thirdSet.generics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResultTest {
+
+    @Test
+    void ok() {
+    }
+
+    @Test
+    void err() {
+    }
+
+    @Test
+    void of() {
+    }
+
+    @Test
+    void map() {
+    }
+
+    @Test
+    void mapErr() {
+    }
+
+    @Test
+    void orElse() {
+    }
+
+    @Test
+    void unwrap() {
+    }
+}
