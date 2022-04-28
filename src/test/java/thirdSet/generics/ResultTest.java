edf0ab963f825d56f4325e06c5e4cb53a8e6c20c
rmieckowski <rmieckowski@griddynamics.com>
2022-04-28 20:24:39 +0200

    implement okTest in ResultTest.java, add assertj dependency to pom.xml file

--- src/test/java/thirdSet/generics/ResultTest.java
+++ src/test/java/thirdSet/generics/ResultTest.java
@@ -1,13 +1,32 @@
 package thirdSet.generics;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 class ResultTest {
 
     @Test
-    void ok() {
+    @DisplayName("ok method should return new instance of Return type")
+    void okTest() {
+        Result<String, Exception> actualResult = Result.ok("Java");
+        Class<?> aClass = actualResult.getClass();
+        Field[] actualFields = aClass.getDeclaredFields();
+        List<String> actualFieldNames = new ArrayList<>();
+        for (Field field : actualFields)
+            actualFieldNames.add(field.getName());
+
+        List<String> expectedNamesOfFields = List.of("operationResult", "exception");
+
+        assertEquals("Result", aClass.getSimpleName());
+        assertThat(actualFieldNames.equals(expectedNamesOfFields.toArray()));
     }
 
     @Test
