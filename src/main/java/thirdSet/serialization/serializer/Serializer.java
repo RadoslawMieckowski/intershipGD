785f8ca580bf4df8df9ffb52236f2ba7d56fe142
rmieckowski <rmieckowski@griddynamics.com>
2022-04-29 14:49:52 +0200

    add Serializer.java to serializer directory

---
+++ src/main/java/thirdSet/serialization/serializer/Serializer.java
@@ -0,0 +1,30 @@
+package thirdSet.serialization.serializer;
+
+import java.io.*;
+
+public final class Serializer {
+    private Serializer() {}
+
+    public static <T extends Serializable> void serialize(T object, String filePath) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream))
+        {
+            objectOutputStream.writeObject(object);
+        } catch (IOException exception) {
+            System.out.println("Serialization failed.");
+        }
+    }
+
+    static <T> T deserialize(String filePath) {
+        T retrievedObject = null;
+        try (FileInputStream fileInputStream = new FileInputStream(filePath);
+             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+            retrievedObject = (T) objectInputStream.readObject();
+        } catch (IOException exception) {
+            exception.getMessage();
+        } catch (ClassNotFoundException exception) {
+            exception.getMessage();
+        }
+        return retrievedObject;
+    }
+}
