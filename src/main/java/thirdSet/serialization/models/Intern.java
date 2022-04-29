77b6929461b1ea6299b0c143ebacab2fda0160f3
rmieckowski <rmieckowski@griddynamics.com>
2022-04-29 14:16:41 +0200

    add Mentor.java and Intern.java to models directory

---
+++ src/main/java/thirdSet/serialization/models/Intern.java
@@ -0,0 +1,16 @@
+package thirdSet.serialization.models;
+
+import java.io.Serializable;
+
+public class Intern implements Serializable {
+    private static final long serialVersionUID = 865711341535420413L;
+    private final String name;
+    private final String password;
+    private final Mentor mentor;
+
+    public Intern(String name, String password, Mentor mentor) {
+        this.name = name;
+        this.password = password;
+        this.mentor = mentor;
+    }
+}
---
+++ src/main/java/thirdSet/serialization/models/Mentor.java
@@ -0,0 +1,17 @@
+package thirdSet.serialization.models;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Mentor implements Serializable {
+    private static final long serialVersionUID = 7819523664148063820L;
+    private final String name;
+    private final String password;
+    private List<Intern> interns;
+
+    public Mentor(String name, String password, List<Intern> interns) {
+        this.name = name;
+        this.password = password;
+        this.interns = interns;
+    }
+}
