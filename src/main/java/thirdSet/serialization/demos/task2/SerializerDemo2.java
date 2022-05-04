274f5d7c6f7c6103a2e17161095f4b7a7dd41994
rmieckowski <rmieckowski@griddynamics.com>
2022-05-04 18:16:06 +0200

    add age field, toString method, modify constructor in Client.java , change SerializerDemo2.java

--- src/main/java/thirdSet/serialization/demos/task2/SerializerDemo2.java
+++ src/main/java/thirdSet/serialization/demos/task2/SerializerDemo2.java
@@ -5,7 +5,10 @@ import thirdSet.serialization.serializer.Serializer;
 
 public class SerializerDemo2 {
     public static void main(String[] args) {
-        Client client = new Client("Will", "Willspassword", 48_000);
-        Serializer.serialize(client, "src/main/resources/data/serializationTarget2.ser");
+//        Client client = new Client("Will", "Willspassword", 48_000);
+//        Serializer.serialize(client, "src/main/resources/data/serializationTarget2.ser");
+        Client clientDeserialized = Serializer.deserialize(
+                "src/main/resources/data/serializationTarget2.ser");
+        System.out.println(clientDeserialized);
     }
 }
--- src/main/java/thirdSet/serialization/models/Client.java
+++ src/main/java/thirdSet/serialization/models/Client.java
@@ -6,13 +6,24 @@ public class Client implements Serializable {
     private final String name;
     private final String password;
     private final Integer balance;
-    private Integer age;
+    private final Integer age;
     private static final long serialVersionUID = -8237181389219902238L;
 
-    public Client(String name, String password, Integer balance) {
+    public Client(String name, String password, Integer balance, Integer age) {
         this.name = name;
         this.password = password;
         this.balance = balance;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", balance=" + balance +
+                ", age=" + age +
+                '}';
     }
 }
 
