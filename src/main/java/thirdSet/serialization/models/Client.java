d60c5fe4e37dc2d9935516fb200bfeb5cb6a8909
rmieckowski <rmieckowski@griddynamics.com>
2022-05-04 18:19:49 +0200

    rename field name to username in constr and methods of Client.java

--- src/main/java/thirdSet/serialization/models/Client.java
+++ src/main/java/thirdSet/serialization/models/Client.java
@@ -3,14 +3,14 @@ package thirdSet.serialization.models;
 import java.io.Serializable;
 
 public class Client implements Serializable {
-    private final String name;
+    private final String username;
     private final String password;
     private final Integer balance;
     private final Integer age;
     private static final long serialVersionUID = -8237181389219902238L;
 
-    public Client(String name, String password, Integer balance, Integer age) {
-        this.name = name;
+    public Client(String username, String password, Integer balance, Integer age) {
+        this.username = username;
         this.password = password;
         this.balance = balance;
         this.age = age;
@@ -19,7 +19,7 @@ public class Client implements Serializable {
     @Override
     public String toString() {
         return "Client{" +
-                "name='" + name + '\'' +
+                "username='" + username + '\'' +
                 ", password='" + password + '\'' +
                 ", balance=" + balance +
                 ", age=" + age +
