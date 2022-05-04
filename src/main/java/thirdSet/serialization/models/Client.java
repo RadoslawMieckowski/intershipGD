9a054d68df56da2ec2e8b5c36c3967be89b2eaa4
rmieckowski <rmieckowski@griddynamics.com>
2022-05-04 18:22:40 +0200

    remove password field in Client.java

--- src/main/java/thirdSet/serialization/models/Client.java
+++ src/main/java/thirdSet/serialization/models/Client.java
@@ -4,14 +4,12 @@ import java.io.Serializable;
 
 public class Client implements Serializable {
     private final String username;
-    private final String password;
     private final Integer balance;
     private final Integer age;
     private static final long serialVersionUID = -8237181389219902238L;
 
-    public Client(String username, String password, Integer balance, Integer age) {
+    public Client(String username, Integer balance, Integer age) {
         this.username = username;
-        this.password = password;
         this.balance = balance;
         this.age = age;
     }
@@ -20,7 +18,6 @@ public class Client implements Serializable {
     public String toString() {
         return "Client{" +
                 "username='" + username + '\'' +
-                ", password='" + password + '\'' +
                 ", balance=" + balance +
                 ", age=" + age +
                 '}';
