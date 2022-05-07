c3fdd16ea2ef2bca7fb8a076c6700a545e57c6c6
rmieckowski <rmieckowski@griddynamics.com>
2022-05-07 20:01:27 +0200

    add Website.java to models directory

---
+++ src/main/java/thirdSet/serialization/models/Website.java
@@ -0,0 +1,37 @@
+package thirdSet.serialization.models;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+public class Website implements Serializable {
+private final String name;
+private List<UserExternalizable> userExternalizableList;
+private static final long serialVersionUID = 8372080265705624161L;
+
+    public Website(String name) {
+        this.name = name;
+    }
+
+    public void setUserExternalizableList(List<UserExternalizable> userExternalizableList) {
+        this.userExternalizableList = userExternalizableList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<UserExternalizable> getUserExternalizableList() {
+        return userExternalizableList;
+    }
+
+    @Override
+    public String toString() {
+        return "Website{" +
+                "name='" + name + '\'' +
+                ", userExternalizableList=" +
+                        (userExternalizableList != null ?
+                                Arrays.toString(userExternalizableList.toArray()) : " null") +
+                '}';
+    }
+}
