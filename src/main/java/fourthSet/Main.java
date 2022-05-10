f8e622e179286c771020c774e138db24e132df3d
rmieckowski <rmieckowski@griddynamics.com>
2022-05-10 18:27:34 +0200

    add User.java to fourthSet directory

--- src/main/java/fourthSet/Main.java
+++ src/main/java/fourthSet/Main.java
@@ -29,7 +29,29 @@ public class Main {
             RETURNING *;
             """;
 
-
+    public static List<Users> fetchData() throws SQLException {
+        String SQL_QUERY = "select * from emp";
+        List<Employee> employees = null;
+        try (Connection con = DataSource.getConnection();
+             PreparedStatement pst = con.prepareStatement( SQL_QUERY );
+             ResultSet rs = pst.executeQuery();) {
+            employees = new ArrayList<>();
+            Employee employee;
+            while ( rs.next() ) {
+                employee = new Employee();
+                employee.setEmpNo( rs.getInt( "empno" ) );
+                employee.setEname( rs.getString( "ename" ) );
+                employee.setJob( rs.getString( "job" ) );
+                employee.setMgr( rs.getInt( "mgr" ) );
+                employee.setHiredate( rs.getDate( "hiredate" ) );
+                employee.setSal( rs.getInt( "sal" ) );
+                employee.setComm( rs.getInt( "comm" ) );
+                employee.setDeptno( rs.getInt( "deptno" ) );
+                employees.add( employee );
+            }
+        }
+        return employees;
+    }
    /* Class driverClass;
 
     {
