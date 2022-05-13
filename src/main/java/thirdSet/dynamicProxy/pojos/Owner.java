7356f111f44b6aaa061cf4c4c638bb94908d0079
rmieckowski <rmieckowski@griddynamics.com>
2022-05-13 10:26:13 +0200

    add lombook dependency, remove boiler plate code in Pet.java and Owner.java

--- src/main/java/thirdSet/dynamicProxy/pojos/Owner.java
+++ src/main/java/thirdSet/dynamicProxy/pojos/Owner.java
@@ -1,46 +1,20 @@
 package thirdSet.dynamicProxy.pojos;
 
+import lombok.*;
 import thirdSet.dynamicProxy.interfaces.Printable;
 
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Owner implements Printable {
     private String name;
     private List<Pet> pets;
 
-    public Owner() {
-    }
-
     public Owner(String name) {
         this.name = name;
-    }
-
-    public Owner(String name, List<Pet> pets) {
-        this.name = name;
-        this.pets = pets;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(List<Pet> pets) {
-        this.pets = pets;
-    }
-
-    @Override
-    public String toString() {
-        return "Owner{" +
-                "name='" + name + '\'' +
-                ", pets=" + pets +
-                '}';
     }
 }
--- src/main/java/thirdSet/dynamicProxy/pojos/Pet.java
+++ src/main/java/thirdSet/dynamicProxy/pojos/Pet.java
@@ -1,59 +1,25 @@
 package thirdSet.dynamicProxy.pojos;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import thirdSet.dynamicProxy.interfaces.Printable;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pet implements Printable {
     private String species;
     private String name;
     private boolean canFly;
     private Owner owner;
 
-    public Pet() {
-    }
-
-    public Pet(String species, String name, boolean canFly, Owner owner) {
-        this.species = species;
-        this.name = name;
-        this.canFly = canFly;
-        this.owner = owner;
-    }
-
     public Pet(String species, String name, boolean canFly) {
         this.species = species;
         this.name = name;
         this.canFly = canFly;
-    }
-
-    public String getSpecies() {
-        return species;
-    }
-
-    public void setSpecies(String species) {
-        this.species = species;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isCanFly() {
-        return canFly;
-    }
-
-    public void setCanFly(boolean canFly) {
-        this.canFly = canFly;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
     }
 
     @Override
