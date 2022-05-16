f54484e0759dd96fc05d919abb1f1ff06fbfb644
rmieckowski <rmieckowski@griddynamics.com>
2022-05-16 13:49:21 +0200

    add prepareToInsertSQLValues, addChars, ListToSQLValues methods in CSVReader.java, trim MOCK_DATA.csv do 100 records

--- src/main/java/secondSet/utilities/CSVReader.java
+++ src/main/java/secondSet/utilities/CSVReader.java
@@ -1,14 +1,11 @@
 package secondSet.utilities;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public final class CSVReader {
     private CSVReader(){}
@@ -31,5 +28,35 @@ public final class CSVReader {
                 .map(x -> x.split(","))
                 .forEach(x ->listOfTables.add(x));
         return listOfTables;
+    }
+
+    public static String prepareToInsertSQLValues(String filePath) {
+        List<String> entries = null;
+        String sqlValues = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            entries = reader.lines()
+                    .skip(1)
+                    .map(CSVReader::addChars)
+                    .collect(Collectors.toList());
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ListToSQLValues(entries);
+    }
+
+    private static String addChars(String string) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        return stringBuilder.insert(0, '(')
+                .toString()
+                .replace(",", ",'")
+                .concat("')");
+    }
+
+    private static String ListToSQLValues(List<String> list) {
+        StringBuilder sqlValues = new StringBuilder();
+        list.stream()
+                .forEach(line -> sqlValues.append(line.concat(",\n")));
+        return sqlValues.delete(sqlValues.length() - 2, sqlValues.length())
+                .toString();
     }
 }
