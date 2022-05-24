bb7eb753497ccac20fdfdc079897f162e063dff9
rmieckowski <rmieckowski@griddynamics.com>
2022-05-25 01:07:45 +0200

    fix findManyTest, add equals and hashCode methods in User.java

--- src/main/java/fourthSet/User.java
+++ src/main/java/fourthSet/User.java
@@ -2,6 +2,8 @@ package fourthSet;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
@@ -10,4 +12,18 @@ import lombok.*;
 public class User {
     private int id;
     private String username;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username);
+    }
 }
+
--- src/test/java/fourthSet/utilities/ConnectionMasterTest.java
+++ src/test/java/fourthSet/utilities/ConnectionMasterTest.java
@@ -2,13 +2,19 @@ package fourthSet.utilities;
 
 import fourthSet.DataSource;
 import fourthSet.Statement;
+import fourthSet.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.function.BiFunction;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 class ConnectionMasterTest {
@@ -40,12 +46,51 @@ class ConnectionMasterTest {
     }
 
     @Test
-    void findOne() {
+    void findOne() throws SQLException {
+        ResultSet resultSet = mock(ResultSet.class);
+        String query = Statement.findOneStatement;
+        Object[] args = new Object[] {101, "Radek"};
+        BiFunction<Integer, String, User> mapper =
+                (id, name) -> new User(id, name);
 
+        User expectedUser = new User(101, "Radek");
+
+        when(connection.prepareStatement(anyString(), anyInt(),
+                anyInt())).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.getRow()).thenReturn(1);
+
+        User actualUser = connectionMaster.findOne(query, args, mapper);
+
+        verify(dataSource).getConnection();
+        verify(connection).prepareStatement(anyString(), anyInt(), anyInt());
+        verify(preparedStatement, times(2)).setObject(anyInt(),any());
+        verify(preparedStatement).executeQuery();
+        verify(resultSet).last();
+        verify(resultSet).getRow();
+        verify(resultSet).beforeFirst();
+
+        assertThat(actualUser).isEqualTo(expectedUser);
     }
 
     @Test
-    void findMany() {
+    void findManyTest() throws SQLException {
+        ResultSet resultSet = mock(ResultSet.class);
+        BiFunction mapper = mock(BiFunction.class);
+        String sampleQuery = "SAMPLE QUERY";
+        var args = new Object[]{1, 2};
+
+        User expectedUser1 = new User(1, "USERNAME1");
+        User expectedUser2 = new User(2, "USERNAME2");
+        List<User> expectedResultList = List.of(expectedUser1, expectedUser2);
+
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true, true, false);
+        when(mapper.apply(eq(resultSet.getInt(1)), eq(resultSet.getString(2)))).thenReturn(new User(expectedUser1.getId(), expectedUser2.getUsername()));
+
+        List<User> result = connectionMaster.findMany(sampleQuery, args, mapper);
+
+        assertThat(expectedResultList).isEqualTo(result);
 
     }
 }
