11274205b26ab7056239077c3c36a287a088eee0
rmieckowski <rmieckowski@griddynamics.com>
2022-05-30 13:31:26 +0200

    change MySQLWrapperException.java

--- src/main/java/fourthSet/exceptions/MySQLWrapperException.java
+++ src/main/java/fourthSet/exceptions/MySQLWrapperException.java
@@ -1,2 +1,9 @@
-package fourthSet.exceptions;public class MySQLWrapperException {
+package fourthSet.exceptions;
+
+import java.sql.SQLException;
+
+public class MySQLWrapperException extends RuntimeException{
+    public MySQLWrapperException(String message, SQLException exception) {
+        super(message, exception);
+    }
 }
