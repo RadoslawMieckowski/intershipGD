7f807f671b9f7d3f07c9733d60cb6edbc33e6555
rmieckowski <rmieckowski@griddynamics.com>
2022-05-31 16:08:29 +0200

    add @EqualsAndHashCode annotation in User.java

--- src/main/java/fourthSet/User.java
+++ src/main/java/fourthSet/User.java
@@ -2,28 +2,14 @@ package fourthSet;
 
 import lombok.*;
 
-import java.util.Objects;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 @ToString
+@EqualsAndHashCode
 public class User {
     private int id;
     private String username;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id == user.id && username.equals(user.username);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, username);
-    }
 }
 
